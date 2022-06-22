a9dd864cd8022b1a44b3be47612062e6759717cf
yong <yongpinbong@gmail.com>
2022-06-22 13:41:49 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1 class='text-center font-medium text-[5rem] text-slate-600'>My Simple App</h1>
+      <!-- <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav> -->
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+@import '@/assets/base.css';
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,18 @@
+<script setup>
+</script>
+
+<template>
+  <div class="main mt-10">
+    <h1 class="text-green-500 font-semibold text-center mb-10">Register a User Form</h1>
+    <div class="flex flex-row w-[100vw] justify-center align-middle">
+      <div class="flex flex-col gap-2 w-1/2 p-36 justify-center items-center">
+        <input class="border-2 rounded-md py-1 px-3" type="text" placeholder="First Name">
+        <input class="border-2 rounded-md py-1 px-3" type="text" placeholder="Last Name">
+        <button class="px-3 py-1 bg-green-600 text-white rounded-md">Save</button>
+      </div>
+      <div class="flex w-1/2 justify-center items-center">
+        hello
+      </div>
+    </div>
+  </div>
+</template>
